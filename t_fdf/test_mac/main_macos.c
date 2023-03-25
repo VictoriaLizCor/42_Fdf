1e009184a0751e629d6fd1255338691d2bf231cb
lilizarr <lilizarr@student.42wolfsburg.de>
2023-03-25 16:47:01 +0100

    adding stuff

--- t_fdf/test_mac/main_macos.c
+++ t_fdf/test_mac/main_macos.c
@@ -6,12 +6,12 @@
 /*   By: lilizarr <lilizarr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/25 11:23:56 by lilizarr          #+#    #+#             */
-/*   Updated: 2023/03/25 12:03:13 by lilizarr         ###   ########.fr       */
+/*   Updated: 2023/03/25 16:10:27 by lilizarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"../minilibx_macos/mlx.h"
-
+#include "../lib/libft/libft.h"
 
 typedef struct	s_data {
 	void	*img;
@@ -37,11 +37,14 @@ int	main(void)
 	t_data	img;
 
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	img.img = mlx_new_image(mlx, 1920, 1080);
+	mlx_win = mlx_new_window(mlx, 1080, 720, "Hello world!");
+	img.img = mlx_new_image(mlx, 1080, 720);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 								&img.endian);
-	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	my_mlx_pixel_put(&img, 100, 100, 0x00FFFFF);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 150, 150);
 	mlx_loop(mlx);
+	mlx_destroy_image(mlx, img.img);
+	mlx_destroy_window(mlx, mlx_win);
+	exit(0);
 }
